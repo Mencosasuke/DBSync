ae78816b85bb236305fd692c483794690f6c743a
David Mencos <mencosdf@hotmail.com>
2015-09-14 00:55:19 -0600

    Muestra los registros de PostgreSQL. Cambio en comportamiento de estilo del menu principal.

--- DBSync/Controllers/Home/HomeController.cs
+++ DBSync/Controllers/Home/HomeController.cs
@@ -5,38 +5,14 @@ using System.Web;
 using System.Web.Mvc;
 
 using DBSync.Connection;
-//using System.Diagnostics;
 
 namespace DBSync.Controllers.Home
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
 
         public ActionResult Index()
         {
-            //MySQLConnection conexionMySql = new MySQLConnection();
-            //PostgreSQLConnection conexionPostgreSQL = new PostgreSQLConnection();
-
-            //if (conexionPostgreSQL.InsertarContacto() > 0)
-            //{
-            //    Debug.WriteLine("Al parecer si hizo el insert :)");
-            //}
-            //if (conexionMySql.InsertarContacto() > 0)
-            //{
-            //    Debug.WriteLine("Al parecer si hizo el insert :)");
-            //}
-
-            //if (conexionPostgreSQL.EliminarContacto("2567648320101") > 0)
-            //{
-            //    Debug.WriteLine("Al parecer si borro la tupla :)");
-            //}
-            //if (conexionMySql.EliminarContacto("2567648320101") > 0)
-            //{
-            //    Debug.WriteLine("Al parecer si borro la tupla :)");
-            //}
-            
             return View();
         }
 
